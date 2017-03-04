d4b8e1f63dd64fdf3154c62cf3d126d97e24db35
Daniel Wilson <dwilson@csumb.edu>
2017-03-04 03:44:57 +0000

    fixing database crash

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import flask_sqlalchemy, app, os
 
 
 app.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+#app.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chatapp:chatapp@localhost/chatdb'
 db = flask_sqlalchemy.SQLAlchemy(app.app)
 
 class Message(db.Model):
